fd2b2c6a196b648bb725fecd4442f284a9072b5e
Dalvir <dalvir.suri@cybrilla.com>
2012-09-29 13:00:46 +0530

    Added Postgres db

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120925074607) do
+ActiveRecord::Schema.define(:version => 20120928111417) do
 
   create_table "clients", :force => true do |t|
     t.string   "name"
     t.text     "address"
     t.string   "contact_person_name"
-    t.integer  "contact_person_number"
+    t.string   "contact_person_number"
     t.string   "contact_person_email"
     t.datetime "created_at",            :null => false
     t.datetime "updated_at",            :null => false
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,13 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Client.create(address: 'Bangalore', contact_person_email: 'client1@client.com', contact_person_name: 'XYZ' , contact_person_number: '0987654321', name: 'Client1');
+#Client.create(address: 'Bangalore', contact_person_email: 'client1@client.com', contact_person_name: 'XYZ' , contact_person_number: '0987654321', name: 'Client1');
  
-Client.create(address: 'Bangalore', contact_person_email: 'client2@client.com', contact_person_name: 'XYZ' , contact_person_number: '0987654321', name: 'Client2'); 
+#Client.create(address: 'Bangalore', contact_person_email: 'client2@client.com', contact_person_name: 'XYZ' , contact_person_number: '0987654321', name: 'Client2'); 
 
 Status.create(description: 'Created', name: 'Created');
 Status.create(description: 'Pending', name: 'Pending');
-Status.create(description: 'Received', name: 'Received');
+#Status.create(description: 'Received', name: 'Received');
 Status.create(description: 'Closed', name: 'Closed');
 
 
