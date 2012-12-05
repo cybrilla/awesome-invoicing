4e836dd1328baa5338fbec69543943a2b7e746aa
Dalvir <dalvir.suri@cybrilla.com>
2012-12-05 16:15:44 +0530

    rails capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,8 +6,8 @@ set :deploy_via, :remote_cache
 set :rake, 'bundle exec rake'
 
 
-#set :rvm_ruby_string, 'ree@rails3'                     # Or:
-set :rvm_ruby_string, ENV['GEM_HOME'].gsub(/.*\//,"") # Read from local system
+set :rvm_ruby_string, 'ree@rails3'                     # Or:
+#set :rvm_ruby_string, ENV['GEM_HOME'].gsub(/.*\//,"") # Read from local system
 set :rvm_type, :user  # Don't use system-wide RVM
 
 require "rvm/capistrano"                               # Load RVM's capistrano plugin.
